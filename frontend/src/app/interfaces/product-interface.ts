d7ec64993f50ba85fa03bfcf8f22538bfdcb2b3e
Fabrício Macena <fabriciomacena03@gmail.com>
2024-04-11 20:29:16 -0300

    inteface dos produtos adicionada

---
+++ frontend/src/app/interfaces/product-interface.ts
@@ -0,0 +1,11 @@
+import { UUID } from "crypto";
+
+export interface ProductInterface {
+  id: UUID,
+  name: string,
+  amount: number,
+  price: number,
+  category: string,
+  supplier: string,
+  insertionDate: Date
+}
